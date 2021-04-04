499114f8e39be825858d9d4320f88703a33d947f
Hyejin Lee <hichjay@gmail.com>
2021-04-05 00:14:28 +0900

    style: hover기능, active기능 완료 w/ javascript

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-
 // Add active class to the current button (highlight it)
-var header = document.getElementById("myDIV");
-var btns = header.getElementsByClassName("btn");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function() {
-  var current = document.getElementsByClassName("active");
-  current[0].className = current[0].className.replace(" active", "");
-  this.className += " active";
+var listMenu = document.getElementById("list-menu");
+var listMenuItem = listMenu.getElementsByClassName("list-item");
+console.log(listMenu);
+console.log(listMenuItem);
+for (var i = 0; i < listMenuItem.length; i++) {
+  listMenuItem[i].addEventListener("click", function () {
+    var current = document.getElementsByClassName("list-item-active");
+    console.log("current", current);
+    current[0].className = current[0].className.replace(
+      " list-item-active",
+      ""
+    );
+    this.className += " list-item-active";
   });
 }
