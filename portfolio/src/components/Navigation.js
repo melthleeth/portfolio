114061ac8e05ed5297e1a73b4d4bc536ba27ca37
Hyejin Lee <hichjay@gmail.com>
2021-06-18 23:48:43 +0900

    Navigation component 업데이트

--- portfolio/src/components/Navigation.js
+++ portfolio/src/components/Navigation.js
@@ -10,7 +10,14 @@ const Navigation = () => {
   };
 
   return (<header>
-      <NavLink><span>Home</span></NavLink>
+      <NavLink to="/home" activeClassName={classes.active}><span>HOME</span></NavLink>
+      <NavLink to="/about" activeClassName={classes.active}><span>ABOUT</span></NavLink>
+      <NavLink to="/skills" activeClassName={classes.active}><span>SKILLS</span></NavLink>
+      <NavLink to="/projects" activeClassName={classes.active}><span>PROJECTS</span></NavLink>
+      <NavLink to="/experience" activeClassName={classes.active}><span>EXPERIENCE</span></NavLink>
+      <NavLink to="/education" activeClassName={classes.active}><span>EDUCATION</span></NavLink>
+      <NavLink to="/certificate" activeClassName={classes.active}><span>CERTIFICATE</span></NavLink>
+      <NavLink to="/contact" activeClassName={classes.active}><span>CONTACT</span></NavLink>
   </header>);
 }
 
